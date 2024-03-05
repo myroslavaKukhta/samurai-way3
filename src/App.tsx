4d7744b870468c13af5f88c1ca2642c31f47cc74
myroslavaKukhta <miroslavakukhta@gmail.com>
2024-03-05 21:24:30 +0200

    Les14_add_module-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import './App.css';
-import picture from './picture.webp';
-import logo from './logo.svg';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 function App() {
   return (
       <div className="app-wrapper">
-          <header className="header">
-              <img src={logo} alt='logo'/>
-          </header>
-          <nav className="nav">
-              <div>
-                  <a>Profile</a>
-              </div>
-              <div>
-                  <a>Messages</a>
-              </div>
-              <div>
-                  <a>News</a>
-              </div>
-              <div>
-                  <a>Music</a>
-              </div>
-              <div>
-                  <a>Settings</a>
-              </div>
-          </nav>
-          <div className="content">
-              <img src={picture} alt='logo'/>
-              Main content
-          </div>
-          <footer className="footer">
-              Footer
-          </footer>
+         <Header/>
+         <Navbar/>
+          <Profile/>
+          <Footer/>
       </div>
   );
 }
