a7aa9b93a635932f7a610d651e1730d0e0dc9605
myroslavaKukhta <miroslavakukhta@gmail.com>
2024-03-06 13:26:00 +0200

    Les18_add component Dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer/Footer";
+import Dialogs from "./components/Dialogs/Dialogs";
+import s from "./App.module.css"
 
 function App() {
   return (
-      <div className="app-wrapper">
+      <div className={s.appContent}>
          <Header/>
          <Navbar/>
-          <Profile/>
+          <div className={s.content}>
+              <Dialogs/>
+          </div>
+          {/*<Profile/>*/}
           <Footer/>
       </div>
   );
