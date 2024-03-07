54c5284e55bb3b14069d4820dd6abb2bae1b3486
myroslavaKukhta <miroslavakukhta@gmail.com>
2024-03-07 11:03:43 +0200

    Les_20_21 Add NavLinks & Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
 
-const Dialogs = () => {
+
+interface DialogsProps {
+    message?: string;
+}
+
+const Dialogs: React.FC<DialogsProps> = (props) => {
     return (
         <div className={s.dialogs}>
-Dialogs
+            <div className={s.dialosItem}>
+                <div className={s.dialog}>
+                    Horse
+                </div>
+                <div className={s.dialog}>
+                    Officer
+                </div>
+                <div className={s.dialog}>
+                    Pawn
+                </div>
+            </div>
+            <div className={s.messages}>
+            <div className={s.message}>Hi!</div>
+            <div className={s.message}>Would you like to play chess with me?</div>
+            <div className={s.message}>Hello, I`d like to play!</div>
+        </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
+
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='/profile' className={s.item}>Profile</a>
+                <NavLink to='/profile' className={s.item}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href='/dialogs' className={s.item}>Messages</a>
+                <NavLink to='/dialogs' className={s.item}>Messages</NavLink >
             </div>
             <div className={s.item}>
-                <a href='/play' className={s.item}>Play</a>
+                <NavLink to='/play' className={s.item}>Play</NavLink>
             </div>
             <div className={s.item}>
-                <a href='/games' className={s.item}>Games</a>
+                <NavLink to='/games' className={s.item}>Games</NavLink>
             </div>
             <div className={s.item}>
-                <a href='/settings' className={s.item}>Settings</a>
+                <NavLink to='/settings' className={s.item}>Settings</NavLink>
             </div>
         </nav>
     );
